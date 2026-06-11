f496410728796bf32df465c62b0f61b3092ba454
pbangiola <paulbanj@gmail.com>
2026-06-11 14:19:07 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/nexttask', // <-- Add your exact repository name here
   images: {
     unoptimized: true,
   },
