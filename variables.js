e9aacd65aa8f47b2547032ad160ef69050702aad
pbangiola <paulbanj@gmail.com>
2026-08-03 23:38:33 -0500

    Initialize global variables for user and session
    Initialize global variables for user and session management, including userId and sessionId with localStorage support.

--- variables.js
+++ variables.js
@@ -1,1 +1,46 @@
+// --- Config ---
+// declare all front-end global variables
+
+// --- User ID & Initialization ---
+//// load existing session id from memory
+let userId = localStorage.getItem('taskSorteruserId');
+//create new sessionid if necessary
+if (!userId) {
+    userId = 'session_' + Math.random().toString(36).substring(2, 9);
+    localStorage.setItem('taskSorteruserId', sessionId);
+}
+
+// --- Project ID & Initialization ---
+//// declare project id
+let projectId = null;
+
+// --- Session ID & Initialization ---
+//// load existing session id from memory
+let sessionId = localStorage.getItem('taskSorterSessionId');
+//create new sessionid if necessary
+if (!sessionId) {
+    sessionId = 'session_' + Math.random().toString(36).substring(2, 9);
+    localStorage.setItem('taskSorterSessionId', sessionId);
+}
+
+
+// setup tasklist variables
+let currentSortRawTasks = [];
+let sortedTasks = []; // Array: { name, estimatedTime, actualTimeMs, timestamps: {} }
+let currentTaskIndex = 0;
+
+// Global tracking timestamps (saved in state, hidden from UI)
+let sessionStartTimestamp = new Date();
+let setupStartTime = 0;
+let workStartTime = null;
+let currentStepStartTimes = null;
+let availabletime = 0;
+let hardstop = 0;
+let hardstop_reason = "";
+let spareTime = 0; 
+let cumulativestimatedtime = 0;
+
+const MAX_TASK_MINUTES = 30;
+//sets maximum task length
+
 
