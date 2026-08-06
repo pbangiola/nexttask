5e17c4ef70915a57a518f9238e3b7626e6e53d02
pbangiola <paulbanj@gmail.com>
2026-08-06 11:35:14 -0500

    Delete js/core/config.js

--- js/core/config.js
+++
@@ -1,8 +0,0 @@
-// Application-wide configuration values.
-//
-// During the incremental refactor, existing code may continue using its
-// current constants until each feature is migrated to this shared namespace.
-
-window.TaskSorterConfig = Object.freeze({
-    MAX_TASK_MINUTES: 30
-});
